70f31b9eb135c779a336022b5ea6dc45bc40a903
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-13 11:15:34 +0100

    add transactions data

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,28 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }}<span>${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
+    <!-- <li class="minus">
+      Cash <span>-$400</span><button class="delete-btn">x</button>
+    </li>
+    <li class="plus">
+      Paycheck <span>$800</span><button class="delete-btn">x</button>
+    </li> -->
+  </ul>
+</template>
+
+<script setup>
+const transactions = [
+  { id: 1, text: "Computer", amount: -199.99 },
+  { id: 2, text: "Salary", amount: 299.99 },
+  { id: 3, text: "Tea", amount: -4.5 },
+  { id: 4, text: "Camera", amount: 150 },
+];
+</script>
